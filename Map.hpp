f87e605b9fc191f762c44e9bcec131494c22f69b
Christian Mcborrough <christianmcborrough@gmail.com>
2021-10-25 19:43:11 -0400

    currently moving namespaces

--- Map.hpp
+++ Map.hpp
@@ -6,14 +6,12 @@
 #include <initializer_list>
 
 using namespace std;
-
+class  cs540{
 template <typename Key_T, typename Mapped_T>
 class Map{
 	public:
 	//constructor_and_assignments:
-		struct ValueType{
-			pair<const Key_T, Mapped_T>types;
-		};
+		typedef pair<const Key_T, Mapped_T> ValueType;
 		struct node{
 			Key_T key;
 			Mapped_T val;
@@ -27,12 +25,12 @@ class Map{
 		float probability;
 		int maxLevel;
 		Map(): probability(0.5), maxLevel(16){
-			//*****Chance to segfault for adding uninitialised data
+			//Chance to segfault for adding uninitialised data
 			head = nullptr;
 			tail = nullptr;
-			for(size_t i = 0; i < head->forward.size(); ++i){
-				head->forward[i] = tail;
-			}
+			//for(size_t i = 0; i < head->forward.size(); ++i){
+			//	head->forward[i] = tail;
+			//}
 		}
 		Map(const Map& m): probability(0.5), maxLevel(16){
 			if(m->head != nullptr){
@@ -79,34 +77,8 @@ class Map{
 			}
 
 		}
-		void insert(Key_T key, Mapped_T val){
-			key = -1;
-			val = 1;
-			key++;
-		}
-		Map(initializer_list<pair<const Key_T, Mapped_T>> l): probability(0.5), maxLevel(16){
-			//Potentially could set the head and tail to the same node and perhaps create an infinte loop?
-			//Key_T headKey = numeric_limits<Key_T>::min();
-			head = new node(l.begin()->first, l.begin()->second, maxLevel);
-			auto lastElem = l.end()-sizeof(l.begin());
-			tail = new node(lastElem->first, lastElem->second,maxLevel);
-			for(size_t i = 0; i <  head->forward.size(); ++i){
-				head->forward[i] = tail;
-			}
-			pair<Key_T,Mapped_T> prev;
-			for(auto curr : l){
-				if(head->key != curr.first && head->val != curr.second){
-					insert(curr.first, curr.second);
-					cout << "inserted key:" << curr.first << " val: " << curr.second <<  endl;
-					prev = curr;
-				}
-			}
-			tail = new node(prev.first, prev.second, maxLevel);
-		}
-		~Map(){
-			delete head;
-			delete tail;
-			cout << "Map has been destroyed" <<endl;
+		node* makeNode(Key_T key, Mapped_T val, int level){
+			return new node(key,val,level);
 		}
 		Key_T randomLevel(){
 			Key_T v = 1;
@@ -117,7 +89,7 @@ class Map{
 		}
 		Key_T nodeLevel(const vector<node*> v){
 			Key_T currentLevel = 1;
-			Key_T tailKey = numeric_limits<Ket_T>::max();
+			Key_T tailKey = numeric_limits<Key_T>::max();
 			if(v[0]->key == tailKey){
 				return currentLevel;
 			}
@@ -130,6 +102,12 @@ class Map{
 			}
 			return currentLevel;
 		}
+
+		~Map(){
+			delete head;
+			delete tail;
+			cout << "Map has been destroyed" <<endl;
+		}
 		void print(){
 			node* list = head;
 			int lineLength = 1;
@@ -137,7 +115,7 @@ class Map{
 			while(list->forward[0] != nullptr){
 				cout << "value: " << list->forward[0]->value
 				<<", key: "<< list->fowrad[0]->key
-				<<", level: " nodeLevel(list->forward);
+				<<", level: " << nodeLevel(list->forward);
 
 				list = list->forward[0];
 				if(list->forward[0] != nullptr) cout<<" :";
@@ -145,23 +123,40 @@ class Map{
 			}
 			cout << "}" << endl;;
 		}
+		void erase(Key_T key){
+
+		}
 	//size:
 		size_t size() const;
 		bool empty() const;
 	//nested_classes:
-		class Skip_list{
-			public:
-				node* find(Key_T key);
-				void erase(Key_T key);
-				node* makeNode(Key_T key, Mapped_T val, int level);
-		};
 		class Iterator{
-			public:
-				Iterator(const Iterator& addr);
-				~Iterator();
-				Iterator& operator=(const Iterator& addr);
-				Iterator& operator++();
-				Iterator& operator++(int);
+		 	public:
+				node* itr;
+				Iterator(node* n){
+					itr = n;
+				}
+				Iterator(const Iterator& addr){
+					itr = addr.itr;
+				}
+				~Iterator(){
+					delete itr;
+				}
+				Iterator& operator=(const Iterator& addr){
+					itr = addr.itr;
+					return *this;
+				}
+				Iterator& operator++(){
+					itr = itr->forward[0];
+					return this;
+				}
+				Iterator& operator++(int counter){
+					while(counter > 0 && itr !=  itr->forward[0]){
+						itr = itr->forward[0];
+						counter--;
+					}
+					return this;
+				}
 				Iterator& operator--();
 				Iterator& operator--(int);
 				ValueType& operator*() const;
@@ -200,20 +195,102 @@ class Map{
 				const ValueType* operator->() const;
 		};
 	//Iterators:
-		Iterator begin();
-		Iterator end();
+		Iterator begin(){
+			this->itr = head;
+			return this;
+		}
+		Iterator end(){
+			this->itr = tail;
+			return this;
+		}
 		ConstIterator begin() const;
 		ConstIterator end() const;
 		ReverseIterator rbegin();
 		ReverseIterator rend();
 	//Element Access:
-		Iterator find(const Key_T& addr);
+		Iterator find(const Key_T& k){
+			node* x = head;
+			unsigned int currMax = nodeLevel(head->forward);
+			for(unsigned int i = currMax; i-- > 0;){
+				while(x->forward[i] != nullptr && x->forward[i]->key < k){
+					x = x->forward[i];
+				}
+			}
+			x = x->forward[0];
+			Iterator ret (nullptr);
+			if(x->key == k){
+				ret = Iterator(x);
+			} else{
+				ret = Iterator(nullptr);
+			}
+			return ret;
+		}
 		const Iterator find(const Key_T& addr) const;
 		Mapped_T &at(const Key_T &) const;
 		Mapped_T &operator[](const Key_T &);
 	//modifiers:
-		pair<Iterator, bool> insert(const ValueType &){
-			
+		pair<Iterator, bool> insert(const ValueType& elem){
+			node * target = nullptr;
+			if(head == nullptr){
+				head = new node(elem.first, elem.second, maxLevel);
+				Key_T tailKey = numeric_limits<Key_T>::max();
+				Mapped_T tailVal = numeric_limits<Mapped_T>::max();
+				tail = new node(tailKey, tailVal, maxLevel);
+				for(size_t i = 0; i < head->forward.size(); ++i){
+					head->forward[i] = tail;
+				}
+				Iterator ret(head);
+				pair<Iterator, bool> actualret(ret,1);
+				return actualret;
+			}
+			Iterator itr = find(elem.first).itr;
+			if(itr.itr != nullptr){
+				pair<Iterator, bool> ret(itr, 0);
+				return ret;
+			}
+			vector<node*> update(head->forward);
+			Key_T currMax = nodeLevel(head->forward);
+			target = head;
+			Key_T skey = elem.first;
+			for(unsigned int i = currMax; i-- > 0;){
+				while(target->forward[i] != nullptr && target->forward[i]->key < skey){
+					target = target->forward[i];
+				}
+				update[i] = target;
+			}
+			target = target->forward[0];
+			int newNodeLevel = 1;
+			if(target->key != skey){
+				newNodeLevel = randomLevel();
+				int currentLevel = nodeLevel(update);
+				if(newNodeLevel > currentLevel){
+					for(int i = currentLevel+1; i < newNodeLevel;i++){
+						target->forward[i] = update[i]->forward[i] = target;
+					}
+				}
+			}
+			Iterator retItr = Iterator(target);
+			pair<Iterator,bool> ret (retItr,0);
+			return ret;
+		}
+		//il func
+		Map(initializer_list<pair<const Key_T, Mapped_T>> l): probability(0.5), maxLevel(16){
+			//Potentially could set the head and tail to the same node and perhaps create an infinte loop?
+			//Key_T headKey = numeric_limits<Key_T>::min();
+
+			this->head = new node(l.begin()->first, l.begin()->second, maxLevel);
+			this->tail = new node(l.begin()->first, l.begin()->second, maxLevel);
+			for(size_t i = 0; i <  head->forward.size(); ++i){
+				head->forward[i] = nullptr;
+			}
+			for(auto curr : l){
+				if(head->key != curr.first && head->val != curr.second){
+					ValueType vt({curr.first,curr.second});
+					insert(vt);
+					cout << "inserted key:" << curr.first << " val: " << curr.second <<  endl;
+				}
+			}
+			cout << "Entered il func" <<endl;
 		}
 		template <typename IT_T>
 		void insert(IT_T range_beg, IT_T range_end);
@@ -234,5 +311,5 @@ class Map{
 		bool operator==(const ReverseIterator& addr2);
 		bool operator!=(const ReverseIterator& addr2);
 
+	};
 };
-
--- myTest.cpp
+++ myTest.cpp
@@ -3,7 +3,16 @@
 #include "Map.hpp"
 
 int main(){
-	Map <int,string> m{{1,"poopoo"},{2,"peepee"}};
+	cout << "yo wassup" <<endl;
+	//Map <int,string> m{{1,"poopoo"},{2,"peepee"}};
+	Map <int, string> m1;
+	const pair <int,string> input1 = {1,"peepee"};
+	pair <Map<int,string>::Iterator,bool> iresults = m1.insert(input1);
+	cout << iresults.second <<endl;
+	cout<< "inited map" <<endl;
+	//const int key = 1;
+	cout << "plz print something" <<endl;
+	//cout << m.find(key).itr->val <<endl;
 	//m.~Map();
 	return 0;
 }
